e13ef3074392bdae20c4ba709e2abd1a5d8b9136
rick2003 <77014403+rick2003@users.noreply.github.com>
2022-01-11 10:22:15 +0100

    Create drawable.hpp
    moet nog opgesplit worden als uitgebreidt is

---
+++ drawable.hpp
@@ -0,0 +1,20 @@
+#ifndef DRAWABLE_HPP
+#define DRAWABLE_HPP
+
+#include <SFML/Graphics.hpp>
+
+class drawable {
+private:
+    sf::Renderwindow & window;
+    sf::Vector2f position;
+    sf::Vector2f size; 
+    //sf::Sprite image; 
+public:
+    drawable(sf::Renderwindow & window, sf::Vector2f position, sf::Vector2f size):
+        window(window), position(position), size(size)
+    virtual bool overlaps() = 0; 
+    virtual void draw() = 0; 
+    sf::Vector2f get_position(){return position;}
+    sf::Vector2f get_size(){return size;}
+};
+#endif
