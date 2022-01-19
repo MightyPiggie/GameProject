6543cecf94d106a623539883e0242e76ec0b52e1
MightyPiggie <degamendekoe@gmail.com>
2022-01-19 10:18:37 +0100

    Second merge

--- game_screen.hpp
+++ game_screen.hpp
@@ -1,6 +1,6 @@
 #ifndef GAME_SCREEN_HPP
 #define GAME_SCREEN_HPP 
-
+#include "obstacle.hpp"
 
 class gamewindow{
 private:
@@ -9,11 +9,6 @@ private:
     unsigned int ticks = 0;
     sf::RenderWindow window{ sf::VideoMode( width,height,32), "SFML window" , sf::Style::Fullscreen};
     state state_t;
-<<<<<<< HEAD
-
-public:
-
-=======
     //uint16_t score = 0;
     uint16_t highscore;
     uint16_t coins;
@@ -22,7 +17,6 @@ public:
         return read_values(coins, highscore);
     }
 public:
->>>>>>> origin/scherm-loop-grid
     void run(){
         //window settingssettings
         window.setKeyRepeatEnabled(false);
@@ -34,11 +28,7 @@ public:
         ///Knoppen voor de game window.
         buttons quit_gamewindow(window,  {30,30}, {120,60},[this](){window.close();},"Quit", sf::Color(163 , 235 , 177));
         buttons back_to_menu_from_gamewindow(window, {30, 120}, {120, 60},[this](){state_t = MENU;},"Menu", sf::Color(163 , 235 , 177));
-<<<<<<< HEAD
-
-=======
         buttons display_coins(window, {float(width) - 200, 40}, {120, 60}, [this](){state_t = SHOP;},"coins", sf::Color(163 , 235 , 177));
->>>>>>> origin/scherm-loop-grid
         ///Knoppen voor de menu window
         buttons start_game(window, Vector2f_from_unsigned_ints(width/2 - 60, height - height/5), {120,60},[this](){state_t = GAME;},"Start", sf::Color(163 , 235 , 177));
 
@@ -47,11 +37,7 @@ public:
         window_part right(window, {float(width)*3/4, 0}, {float(width)/4.f, float(height)} , sf::Color(33 , 182 , 168));
         window_part game_window(window,{float(width)/4.f, 0}, {float(width)/2.f, float(height)}, sf::Color(24 , 165 , 88));
         player player1 {window , sf::Vector2f{ 960.0, 960.0 }, sf::Vector2f{ 0.0, 0.0 }, "res/sprites/player_sprite.png" , width , height};
-<<<<<<< HEAD
         obstacle test1 {window, sf::Vector2f{1020.0, 0.0}, sf::Vector2f{59.0, 59.0}, "res/sprites/Tree.png"};
-=======
-        obstacle test1 {window, sf::Vector2f{960.0, 0.0}, sf::Vector2f{60.0, 60.0}};
->>>>>>> origin/scherm-loop-grid
         //initialistie menu
         menu Menu(window, {0,0}, Vector2f_from_unsigned_ints(width, height));
 
@@ -63,11 +49,7 @@ public:
             switch (state_t) {
                 case GAME: {
                     //
-<<<<<<< HEAD
-                    objects = {&left,&right, &quit_gamewindow, &back_to_menu_from_gamewindow, &player1, &test1};
-=======
                     objects = {&left,&right, &quit_gamewindow, &back_to_menu_from_gamewindow, &player1, &test1, &display_coins};
->>>>>>> origin/scherm-loop-grid
                     for(auto object : gameobjects){
                         object->lower(1);
                         }
@@ -77,11 +59,7 @@ public:
                         ticks = 0;
                         }
                     }
-<<<<<<< HEAD
-=======
                     display_coins.update_coins(coins);
-                }
->>>>>>> origin/scherm-loop-grid
                     break;
                 case MENU: {
                     objects = {&Menu, &start_game};
