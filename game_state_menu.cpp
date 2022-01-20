dbb1605aa79dd4cba834bd0cb8ed14e34a9348da
SjaakyNewton <74775988+SjaakyNewton@users.noreply.github.com>
2022-01-20 20:11:42 +0100

    Geluid toegevoegd basic
    Geluid aan het toepassen in de menu window. Dit is nu werkend als je een pauze erbij zet

--- game_state_menu.cpp
+++ game_state_menu.cpp
@@ -3,8 +3,9 @@
 //
 
 #include "game_state_menu.hpp"
-
-
+#include <iostream>
+#include <SFML/Audio.hpp>
+#include <stdlib.h>
 
 //#include "game_screen.hpp" ///Moet deze wel?
 
@@ -22,29 +23,35 @@ game_state_menu::game_state_menu(sf::RenderWindow & window,
                                 shop_button_in_menu_window(window,  50, vector2f_from_unsigned_ints(width/2 + 150, height/2 + 350),[&](){state_t = SHOP;},"Shop", sf::Color(163 , 235 , 177)),
 
                                 title_in_menu_window(window,vector2f_from_unsigned_ints(width/2-250, height/2 - 100),"Levensmoedige vogel",60,sf::Color(163 , 235 , 177)),
-                                player_icon_menu_window(window, vector2f_from_unsigned_ints(width/2 -30,height/2 + 80),sprite_files_map["chicken_sprite"])
+                                player_icon_menu_window(window, vector2f_from_unsigned_ints(width/2 -30,height/2 + 80),sprite_files_map["chicken_sprite"]),
+                                title_in_menu_window_sound_test(window, 50,vector2f_from_unsigned_ints(width/2-250,height/2),
+                                                     [](){
+                                                         sf::SoundBuffer buffer;
+                                                         sf::Sound sound;
+                                                         if(!buffer.loadFromFile("res/sounds/menu-window-title.wav")){
+                                                             std::cout << "Error\n";
+                                                         }
+                                                         sound.setBuffer(buffer);
+                                                         std::cout << "speel geluid\n";
+                                                         sound.play();
+                                                         /// Door hem hier even te laten slapen komt die wel door. Alleen stopt die nu wel met de rest laten werken...
+                                                         sf::sleep(sf::seconds(3));
+                                                         },
+                                                     "Test geluid",
+                                                     sf::Color(163 , 235 , 177))
                                 {
                                 rect.setSize(size);
                                 rect.setPosition(0,0);
                                 rect.setFillColor(sf::Color(17 , 101 , 48));
 
-                                ///Knoppen voor de menu window //TODO FIX THIS
-                                //        buttons title_in_menu_window(window, Vector2f_from_unsigned_ints(width/2-250,height/2),{210,10},
-                                //                                     [](){
-                                //                                         sf::SoundBuffer buffer;
-                                //                                         sf::Sound sound;
-                                //                                         buffer.loadFromFile("res/sounds/menu-window-title.wav");
-                                //                                         sound.setBuffer(buffer);
-                                //                                         std::cout << "speel geluid\n";
-                                //                                         sound.play();},
-                                //                                         "Levensmoedige Vogels",
-                                //                                         sf::Color(163 , 235 , 177));
+                                //TODO FIX THIS
                                 objects = {
                                         &quit_in_menu_window,
                                         &start_game,
                                         &shop_button_in_menu_window,
                                         &title_in_menu_window,
-                                        &player_icon_menu_window
+                                        &player_icon_menu_window,
+                                        &title_in_menu_window_sound_test
                                     };
                                 }
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include "game_screen.hpp"
+#include <iostream>
+#include <SFML/Audio.hpp>
+
 
 int main( int argc, char *argv[] ){
+    /// Hier hoor ik geluid. Wordt die na de game_screen game gezet in de main hoor ik geen geluid.
+    /// Zet ik de zelfde instellingen in de game_state_menu.cpp bij de button functie hoor ik ook niks.
+    sf::SoundBuffer buffer;
+    sf::Sound sound;
+    if(!buffer.loadFromFile("res/sounds/menu-window-title.wav")){
+        std::cout << "Error\n";
+    }
+    sound.setBuffer(buffer);
+    std::cout << "speel geluid\n";
+    sound.play();
+
 	game_screen game;
 	game.run();
 }
