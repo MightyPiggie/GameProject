55a2bf735eb1523a41df72bea0348928d286690e
SjaakyNewton <74775988+SjaakyNewton@users.noreply.github.com>
2022-01-20 14:50:03 +0100

    Menu_state gemaakt
    Alle menu opjecten vallen nu in de menu class game_state_menu. Dit is om meer structuur en overzicht te krijgen in de code en waar wat bij hoort.

--- game_screen.cpp
+++ game_screen.cpp
@@ -12,7 +12,7 @@
 #include "window_part.hpp"
 #include "player.hpp"
 #include "builder.hpp"
-#include "menu.hpp"
+#include "game_state_menu.hpp"
 #include "obstacle.hpp"
 
 std::vector<std::string> game_screen::init(){
@@ -35,28 +35,11 @@ void game_screen::run(){
     //knoppen plus wat de knop moet doen
     ///Knoppen voor de game window.
 
-    ///Knoppen voor de menu window //TODO FIX THIS
-    buttons quit_in_menu_window(window,  50, vector2f_from_unsigned_ints(width/2 - 250, height/2 + 350),[this](){window.close();},"Quit", sf::Color(163 , 235 , 177));
-    buttons shop_button_in_menu_window(window,  50, vector2f_from_unsigned_ints(width/2 + 120, height/2 + 350),[this](){state_t = SHOP;},"Shop", sf::Color(163 , 235 , 177));
-//        buttons title_in_menu_window(window, Vector2f_from_unsigned_ints(width/2-250,height/2),{210,10},
-//                                     [](){
-//                                         sf::SoundBuffer buffer;
-//                                         sf::Sound sound;
-//                                         buffer.loadFromFile("res/sounds/menu-window-title.wav");
-//                                         sound.setBuffer(buffer);
-//                                         std::cout << "speel geluid\n";
-//                                         sound.play();},
-//                                         "Levensmoedige Vogels",
-//                                         sf::Color(163 , 235 , 177));
-    buttons test_button(window, 50, {120,60},[this](){state_t = SHOP;},"Testtest", sf::Color(163 , 235 , 177));
-    label title_in_menu_window(window,{300,40},"Levensmoedige vogel",30,sf::Color(163 , 235 , 177));
-    window_part player_icon_menu_window(window, vector2f_from_unsigned_ints(width/2 -30,height/2 + 80),sprite_files_map["chicken_sprite"]);
+
     //knoppen plus wat de knop moet doen
     ///Knoppen voor de game window.
     buttons quit_gamewindow(window, 50,  {40,30},  [this](){window.close();},"Quit", sf::Color(163 , 235 , 177));
     buttons back_to_menu_from_gamewindow(window, 50,  {40, 120}, [this](){state_t = MENU;},"Menu", sf::Color(163 , 235 , 177));
-            ///Knoppen voor de menu window
-    buttons start_game(window, 100,  vector2f_from_unsigned_ints(width/2 - 60, height - height/5), [this](){state_t = GAME;},"Start", sf::Color(163 , 235 , 177));
 
     //initialisatie game
     window_part left(window,{0,0}, {float(width)/4.f, float(height)} , sf::Color(33 , 182 , 168));
@@ -67,7 +50,7 @@ void game_screen::run(){
     obstacle road1 {window, sf::Vector2f{float(width)/4.f, 120.0}, sf::Vector2f{59.0, 59.0} ,NON_OBSTACLE, sprite_files_map["roads_sprite"]};
     label display_coins(window, sf::Vector2f(float(width) - 200.f, 200), std::to_string(coins), 25, sf::Color::Green);
     //initialistie menu
-    menu Menu(window, {0,0}, vector2f_from_unsigned_ints(width, height));
+    game_state_menu menu_state(window, width, height,state_t,sprite_files_map);
 
     //lijst van objecten
     std::vector<drawable*> drawables = {};
@@ -86,8 +69,8 @@ void game_screen::run(){
                 break;
             }
             case MENU: { // TODO Check updateables
-                drawables = {&Menu, &start_game, &title_in_menu_window, &quit_in_menu_window, &shop_button_in_menu_window, &player_icon_menu_window};
-                updateables = {&Menu, &start_game, &title_in_menu_window, &quit_in_menu_window, &shop_button_in_menu_window, &player_icon_menu_window};
+                drawables = {&menu_state};
+                updateables = {&menu_state};
                 break;
             }
             case DEAD: {
