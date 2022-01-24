0b7d4d7321db3a91ab3c42dc5f0f8f75427fc8a8
SjaakyNewton <74775988+SjaakyNewton@users.noreply.github.com>
2022-01-24 17:21:39 +0100

    Shop bijwerken begin ervan
    Shop aangepast dat je nu door de players heen kan scrollen. Pacman en slime erbij gemaakt. Dit moet nog netjes gemaakt worden

--- game_state_shop.cpp
+++ game_state_shop.cpp
@@ -1,24 +1,48 @@
 #include "game_state_shop.hpp"
+#include <iostream>
 
-game_state_shop::game_state_shop(sf::RenderWindow& window,state & state_t, unsigned int width,unsigned int height, std::vector<std::string> & unlocked_players, std::map<std::string, std::string> & sprite_files_map): drawable(window,
-                                                                                                                             {0,0}, {0,0}),
-                                                                                                                             state_t(state_t), unlocked_players(unlocked_players), sprite_files_map(sprite_files_map),
-                                                                                                                             shop_window(window, {0,0}, {float(width), float(height)}, sf::Color(33,182,168)),
-                                                                                                                    quit_gamewindow(window, 50,  {40,30},  [&](){window.close();},"Quit", sf::Color(163 , 235 , 177)),
-                                                                                                                    back_to_menu_from_gamewindow(window, 50,  {40, 120}, [&](){state_t = MENU;},"Menu", sf::Color(163 , 235 , 177)),
-                                                                                                                    first_buy_button(window, 50, {float(width)/2-120.f/2, float(height) - 200.f}, [&](){chosen_player = all_players[1];}, "Buy", sf::Color(163 , 235 , 177)),
-                                                                                                                    first(window, {float(width/4 - 60 * 3),  400.f}, sprite_files_map[all_players[0]]),
-                                                                                                                    second(window, {float(width/2 - 60 * 2.5), 400.f}, sprite_files_map[all_players[1]]),
-                                                                                                                    third(window, {float(width/2*1.5 - 60 * 2.5), 400.f}, sprite_files_map[all_players[2]])
+game_state_shop::game_state_shop(sf::RenderWindow& window,
+                                 state & state_t,
+                                 unsigned int width,
+                                 unsigned int height,
+                                 std::vector<std::string> & unlocked_players,
+                                 std::map<std::string,
+                                 std::string> & sprite_files_map
+                                 ):
+                                 drawable(window,{0,0}, {0,0}),
+                                 state_t(state_t),
+                                 unlocked_players(unlocked_players),
+                                 sprite_files_map(sprite_files_map),
+                                 shop_window(window, {0,0}, {float(width), float(height)}, sf::Color(33,182,168)),
+                                 quit_gamewindow(window, 50,  {40,30},  [&](){window.close();},"Quit", sf::Color(163 , 235 , 177)),
+                                 back_to_menu_from_gamewindow(window, 50,  {40, 120}, [&](){state_t = MENU;},"Menu", sf::Color(163 , 235 , 177)),
+
+                                 /// Shop buttons
+                                 //Todo: Moet nog netjes neergezet worden.
+                                 previeus_player_button(window, 50,{float(width)/2-500.f, float(height) - 200.f},[&](){if(player_scrolling_int <= 0){player_scrolling_int = 0;}else{player_scrolling_int--;} sf::sleep(sf::milliseconds(100));},"prev",sf::Color(163 , 235 , 177)),
+                                 buy_button(window, 50, {float(width)/2-200.f, float(height) - 200.f}, [&](){std::cout << "Unlock player :)";}, "Buy", sf::Color(163 , 235 , 177)),
+                                 equipe_button(window, 50,{float(width)/2+200.f, float(height) - 200.f},[](){std::cout << "Zet als player neer";},"Equip",sf::Color(163 , 235 , 177)),
+                                 next_player_button(window, 50,{float(width)/2+500.f, float(height) - 200.f},[&](){if(player_scrolling_int > players.size()){player_scrolling_int = players.size();}else{player_scrolling_int++;} sf::sleep(sf::milliseconds(100));},"next",sf::Color(163 , 235 , 177)),
+
+                                 ///Steeds players toevoegen aan deze lijst. Deze komen in een vector. Door deze loop je steeds
+                                 chicken(window, {float(width/2 - 360),  400.f}, sprite_files_map[all_players[0]]),
+                                 slime(window, {float(width/2 - 360),  400.f}, sprite_files_map[all_players[1]]),
+                                 pacman(window, {float(width/2 - 360),  400.f}, sprite_files_map[all_players[2]])
 {
-    first.rescale({5,5});
-    second.rescale({5,5});
-    third.rescale({5,5});
-    objects = {&shop_window,&quit_gamewindow,&back_to_menu_from_gamewindow, &first_buy_button,   &first, &second, &third};
+    objects = {&shop_window,&quit_gamewindow,&back_to_menu_from_gamewindow};
+    /// Lijst van players die er zijn? Kan misschien anders
+    players = {&chicken, &slime, &pacman};
+
+    /// Wou het wat aanpassen dat de buy button vervangen wordt door de equip button indien die gekocht is.
+    scrol_objects = {&previeus_player_button, &buy_button, &equipe_button, &next_player_button};
+
 }
 
 void game_state_shop::draw() {
     for(auto &object : objects){
+        object->draw();
+    }
+    for(auto &object : scrol_objects){
         object->draw();
     }
 }
@@ -27,6 +51,13 @@ void game_state_shop::update() {
     for(auto &object: objects){
         object->update();
     }
+    for(auto &object : scrol_objects){
+        object->update();
+    }
+    if(player_scrolling_int > players.size()){
+        player_scrolling_int = players.size();
+    }
+    players[player_scrolling_int]->draw();
 
     sf::Event event{};
     while (window.pollEvent(event)) {}
--- game_state_shop.hpp
+++ game_state_shop.hpp
@@ -11,19 +11,35 @@
 class game_state_shop : public drawable{
 private:
     state & state_t;
-    std::vector<std::string> all_players = {"chicken_sprite", "beta_player_sprite", "chicken_sprite"};
+    /// Lijst players die er zijn om hiermee de sprites ervan op te halen
+    std::vector<std::string> all_players = {"chicken_shop", "slime_shop", "pacman_shop"};
+    ///Lijst players die gekocht zijn?
     std::vector<std::string> & unlocked_players;
     std::map<std::string, std::string> & sprite_files_map;
-    std::vector<window_part> sprites;
+
+    /// Lijst players die er zijn. Alles dus maar dan window_part. Met all_players kun je de juiste sprite pakken.
+    std::vector<window_part*> players;
     std::vector<drawable*> objects;
+    std::vector<drawable*> scrol_objects;
     window_part shop_window;
     buttons quit_gamewindow;
     buttons back_to_menu_from_gamewindow;
-    buttons first_buy_button;
-    window_part first;
-    window_part second;
-    window_part third;
+
+    /// Buttons om door de shop heen te gaan.
+    buttons previeus_player_button;
+    buttons buy_button;
+    buttons equipe_button;
+    buttons next_player_button;
+
+    /// Players die ik heb en in de shop wil tonen
+    window_part chicken;
+    window_part slime;
+    window_part pacman;
     std::string chosen_player;
+
+    /// Waarde waar mee ik door de shop heen kan scrollen van players.
+    unsigned int player_scrolling_int = 0;
+
 public:
     game_state_shop(sf::RenderWindow& window, state & state_t,unsigned int width,unsigned int height, std::vector<std::string> & unlocked_players, std::map<std::string, std::string> & sprite_files_map);
     void draw() override;
