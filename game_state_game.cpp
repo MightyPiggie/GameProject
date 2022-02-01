bc3258c867dce4fa1d28acf936c86732afc52cab
SjaakyNewton <74775988+SjaakyNewton@users.noreply.github.com>
2022-02-01 16:59:25 +0100

    Dingen aangepast na merge
    Laatste dingen aangepast na de merge

--- game_state_game.cpp
+++ game_state_game.cpp
@@ -36,8 +36,7 @@ game_state_game::game_state_game(sf::RenderWindow & window,
     std::shared_ptr<label_player_settings_score> display_score = std::make_shared<label_player_settings_score>(window, sf::Vector2f(float(width) - 250.f, 150), std::to_string(gameSettings.score), 25, sf::Color(163 , 235 , 177), gameSettings, false);
     std::shared_ptr<buttons> quit_gamewindow = std::make_shared<buttons>(window, 50,  vector2f_from_unsigned_ints(40,30),  [&](){if(gameSettings.sound){click_sound.play();} window.close();},"Quit", sf::Color(163 , 235 , 177));
     std::shared_ptr<buttons> back_to_menu_gamewindow = std::make_shared<buttons>(window, 50,  vector2f_from_unsigned_ints(40, 120), [&](){if(gameSettings.sound){click_sound.play();}  state_t = MENU;},"Menu", sf::Color(163 , 235 , 177));
-    // TODO geluid hier bijzetten
-    std::shared_ptr<buttons> pauze_button = std::make_shared<buttons>(window, 40,  vector2f_from_unsigned_ints(float(width) - 300.f,float(height)-200.f),  [&](){click_sound.play(); gameSettings.started = !gameSettings.started; sf::sleep(sf::milliseconds(100));},"Pauze", sf::Color(163 , 235 , 177));
+    std::shared_ptr<buttons> pauze_button = std::make_shared<buttons>(window, 40,  vector2f_from_unsigned_ints(float(width) - 300.f,float(height)-200.f),  [&](){if(gameSettings.sound){click_sound.play();} gameSettings.started = !gameSettings.started; sf::sleep(sf::milliseconds(100));},"Pauze", sf::Color(163 , 235 , 177));
     background = game_window;
     drawables = {left, right, display_coins, display_score, display_highscore, quit_gamewindow, back_to_menu_gamewindow, pauze_button};
 }
